86416cf7dbebc15bd152a64265773ed9d287bfcf
RynTrq <raiyaan22290@iiitd.ac.in>
2026-02-06 18:26:39 +0530

    Implementation of Home page with tradingview widget

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,66 @@
 import {Button} from "@/components/ui/button";
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {
+    HEATMAP_WIDGET_CONFIG,
+    MARKET_DATA_WIDGET_CONFIG,
+    MARKET_OVERVIEW_WIDGET_CONFIG,
+    TOP_STORIES_WIDGET_CONFIG
+} from "@/lib/constants";
 
 const Home = () => {
+    const scriptURL = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
     return (
         <div className="flex min-h-screen home-wrapper">
-          Home
+          <section className="grid w-full gap-8 home-section">
+              <div className="md:col-span-1 xl:col-span-1">
+                  <TradingViewWidget
+                      title="Market OverView"
+                      scriptUrl={`${scriptURL}market-overview.js`}
+                      config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                      height={600}
+                      className="custom-chart"
+                  />
+              </div>
+              <div className="md-col-span xl:col-span-2">
+                  <TradingViewWidget
+                      title="Stock Heatmap"
+                      scriptUrl={`${scriptURL}stock-heatmap.js`}
+                      config={HEATMAP_WIDGET_CONFIG}
+                      height={600}
+                      className="custom-chart"
+                  />
+              </div>
+          </section>
+          <section className="w-full gap-8 home-section">
+              <div>
+                  <TradingViewWidget
+                      title="Advanced Chart"
+                      scriptUrl={`${scriptURL}advanced-chart.js`}
+                      config={MARKET_OVERVIEW_WIDGET_CONFIG}
+                      height={600}
+                      className="custom-chart"
+                  />
+              </div>
+          </section>
+          <section className="grid w-full gap-8 home-section">
+              <div className="h-full md:col-span-1">
+                  <TradingViewWidget
+                      scriptUrl={`${scriptURL}timeline.js`}
+                      config={TOP_STORIES_WIDGET_CONFIG}
+                      height={600}
+                      className="custom-chart"
+                  />
+              </div>
+              <div className="md-col-span xl:col-span-2">
+                  <TradingViewWidget
+                      scriptUrl={`${scriptURL}market-quotes.js`}
+                      config={MARKET_DATA_WIDGET_CONFIG}
+                      height={600}
+                      className="custom-chart"
+                  />
+              </div>
+          </section>
         </div>
     )
 }
